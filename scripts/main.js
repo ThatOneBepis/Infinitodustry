665b8a923d0376a57e9cdea07b9322606873d6db
ThatOneBepis <47719370+ThatOneBepis@users.noreply.github.com>
2020-11-15 12:51:59 +0100

    main.js but more organised

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,21 @@
-require("mechanics/xp");
+//region mechanics
+require("mechanics/xp"); //courtersy of sk7725/xp-lib
+require("mechanics/multi-lib"); //courtersy of younggam/multi-lib
+//endregion
+//region campaign
+require("campaign/research");
+require("campaign/planets/ainamelzung");
+//endregion
+//region units
+require("units/ground/missile-branch");
+require("units/bosses/gods");
+require("units/bosses/cyclers");
+//endregion
+//region turrets
 require("blocks/turrets/blest");
-require("campaign/upgrades");
-require("campaign/items");
-require("campaign/planets/ainara");
+//endregion
+//region "made using multi-lib.js"
+require("blocks/crafters/swissknife");
+require("blocks/crafters/recycle");
+require("blocks/crafters/multikiln");
+//endregion
