2ca9e6aa85afe5bcbb9e14da557accf9db0d6cb4
ThatOneBepis <47719370+ThatOneBepis@users.noreply.github.com>
2020-11-15 12:53:27 +0100

    irrelevant too

--- scripts/campaign/upgrades.js
+++
@@ -1,7 +0,0 @@
-/*const tau1 = extendContent(Block, "tau-branch", {
-  setStats(){BlockType.core.unitCapModifier = +10%},
-  canPlaceOn(){return false},
-  isHidden(){return true}
-});
-
-unviable till modded items can be researched*/
