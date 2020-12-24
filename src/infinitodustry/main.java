e9b88943aaf7c7f2ba2b9f8cb410adc680cc2380
Bepis <bozidar444@gmail.com>
2020-12-24 20:56:45 +0100

    test

---
+++ src/infinitodustry/main.java
@@ -0,0 +1,24 @@
+package infinitodustry;
+
+import arc.*;
+import arc.func.*;
+import arc.util.Log;
+import mindustry.*;
+import mindustry.ctype.*;
+import mindustry.game.EventType.*;
+import mindustry.mod.*;
+import mindustry.mod.Mods.*;
+import infinitodustry.content.*;
+
+import mindustry.mod.Mod;
+
+public class infinitodustry extends Mod{
+
+    @Override
+    public void init(){
+    }
+
+    @Override
+    public void loadContent(){
+    }
+}
